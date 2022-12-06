dedc9ecccbd24d358cb85421d42b57e1c0ec6c49
Victor Gomenza <viguitars738@gmail.com>
2022-12-06 00:19:50 -0300

    Create 04 - Palindrome Checker Project.js

---
+++ JavaScript Algorithms and Data Structures/04 - Palindrome Checker Project.js
@@ -0,0 +1,40 @@
+/* cSpell: disable */
+
+/* Comprobador de palíndromos
+Devuelve true si la cadena proporcionada es un palíndromo.De lo contrario, devuelve false.
+
+Un palíndromo es una palabra o frase que se escribe de la misma manera hacia adelante y hacia atrás, 
+ignorando la puntuación, mayúsculas, minúsculas y espaciado.
+
+Nota: Tendrás que eliminar todos los caracteres no alfanuméricos(puntuación, espacios y símbolos) y 
+convertir todo en mayúsculas o minúsculas para comprobar si hay palíndromos.
+
+Pasaremos cadenas con formatos variables, como racecar, RaceCar y race CAR entre otros.
+
+También pasaremos cadenas con símbolos especiales, como 2A3 * 3a2, 2A3 3a2 y 2_A3 * 3#A2. */
+
+function palindrome(str) {
+
+    const strRegex = /[/_(:/)-., \s+]/g;
+    const newStr = str.replace(strRegex, '').toUpperCase();
+    const reverseStr = newStr
+        .split("")
+        .reverse()
+        .join("");
+    return newStr === reverseStr;
+}
+
+
+console.log(palindrome("eye")); //debe devolver true.
+console.log(palindrome("eye")); //debe devolver true.
+console.log(palindrome("_eye")); //debe devolver true.
+console.log(palindrome("race car")); //debe devolver true.
+console.log(palindrome("not a palindrome")); //debe devolver false.
+console.log(palindrome("A man, a plan, a canal. Panama")); //debe devolver true.
+console.log(palindrome("never odd or even")); //debe devolver true.
+console.log(palindrome("nope")); //debe devolver false.
+console.log(palindrome("almostomla")); //debe devolver false.
+console.log(palindrome("My age is 0, 0 si ega ym.")); //debe devolver true.
+console.log(palindrome("1 eye for of 1 eye.")); //debe devolver false.
+console.log(palindrome("0_0 (: /-\ :) 0-0")); //debe devolver true.
+console.log(palindrome("five|\_/|four")); //debe devolver false.
