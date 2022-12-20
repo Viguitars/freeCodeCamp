98875e2856db3e29237cd898ee725530af74f40a
Victor Gomenza <viguitars738@gmail.com>
2022-12-19 23:42:45 -0300

    Update 08 - Caesars Cipher Project.js

--- JavaScript Algorithms and Data Structures/08 - Caesars Cipher Project.js
+++ JavaScript Algorithms and Data Structures/08 - Caesars Cipher Project.js
@@ -2,8 +2,8 @@
 
 /* Cifrado César
 Uno de los cifrados más simples y conocidos es el cifrado César, también conocido como cifrado por 
-desplazamiento. En un cifrado por desplazamiento los significados de las letras se desplazan por una 
-cantidad determinada.
+desplazamiento. En un cifrado por desplazamiento los significados de las letras se desplazan por 
+una cantidad determinada.
 
 Un uso moderno común es el cifrado ROT13, donde los valores de las letras son desplazados por 13 
 lugares. Así que A ↔ N, B ↔ O y así sucesivamente.
