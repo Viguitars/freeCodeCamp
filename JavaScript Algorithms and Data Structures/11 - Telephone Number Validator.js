5b2a45303b0175fcdbc2442631858b228179b4b4
Victor Gomenza <viguitars738@gmail.com>
2023-01-04 21:30:56 -0300

    Update 11 - Telephone Number Validator.js

--- JavaScript Algorithms and Data Structures/11 - Telephone Number Validator.js
+++ JavaScript Algorithms and Data Structures/11 - Telephone Number Validator.js
@@ -27,39 +27,39 @@ const RGX3 = /^(1\s?)?\d{3}\s?\d{3}\s?\d{4}$/;
 const REGEXES = [RGX1, RGX2, RGX3];
 
 function telephoneCheck(str) {
-    for (let regex of REGEXES) {
-        if (regex.test(str)) {
-            return true;
-        }
+  for (let regex of REGEXES) {
+    if (regex.test(str)) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
-console.log(telephoneCheck("1 555-555-5555"));//true.
-console.log(telephoneCheck("1 (555) 555-5555"));//true.
-console.log(telephoneCheck("5555555555"));//true.
-console.log(telephoneCheck("555-555-5555"));//true.
-console.log(telephoneCheck("(555)555-5555"));//true.
-console.log(telephoneCheck("1(555)555-5555"));//true.
-console.log(telephoneCheck("555-5555"));//false.
-console.log(telephoneCheck("5555555"));//false.
-console.log(telephoneCheck("1 555)555-5555"));//false.
-console.log(telephoneCheck("1 555 555 5555"));//true.
-console.log(telephoneCheck("1 456 789 4444"));//true.
-console.log(telephoneCheck("123**&!!asdf#"));//false.
-console.log(telephoneCheck("55555555"));//false.
-console.log(telephoneCheck("(6054756961)"));//false.
-console.log(telephoneCheck("2 (757) 622-7382"));//false.
-console.log(telephoneCheck("0 (757) 622-7382"));//false.
-console.log(telephoneCheck("-1 (757) 622-7382"));//false.
-console.log(telephoneCheck("2 757 622-7382"));//false.
-console.log(telephoneCheck("10 (757) 622-7382"));//false.
-console.log(telephoneCheck("27576227382"));//false.
-console.log(telephoneCheck("(275)76227382"));//false.
-console.log(telephoneCheck("2(757)6227382"));//false.
-console.log(telephoneCheck("2(757)622-7382"));//false.
-console.log(telephoneCheck("555)-555-5555"));//false.
-console.log(telephoneCheck("(555-555-5555"));//false.
-console.log(telephoneCheck("(555)5(55?)-5555"));//false.
-console.log(telephoneCheck("55 55-55-555-5"));//false.
-console.log(telephoneCheck("11 555-555-5555"));//false.
+console.log(telephoneCheck("1 555-555-5555")); //true.
+console.log(telephoneCheck("1 (555) 555-5555")); //true.
+console.log(telephoneCheck("5555555555")); //true.
+console.log(telephoneCheck("555-555-5555")); //true.
+console.log(telephoneCheck("(555)555-5555")); //true.
+console.log(telephoneCheck("1(555)555-5555")); //true.
+console.log(telephoneCheck("555-5555")); //false.
+console.log(telephoneCheck("5555555")); //false.
+console.log(telephoneCheck("1 555)555-5555")); //false.
+console.log(telephoneCheck("1 555 555 5555")); //true.
+console.log(telephoneCheck("1 456 789 4444")); //true.
+console.log(telephoneCheck("123**&!!asdf#")); //false.
+console.log(telephoneCheck("55555555")); //false.
+console.log(telephoneCheck("(6054756961)")); //false.
+console.log(telephoneCheck("2 (757) 622-7382")); //false.
+console.log(telephoneCheck("0 (757) 622-7382")); //false.
+console.log(telephoneCheck("-1 (757) 622-7382")); //false.
+console.log(telephoneCheck("2 757 622-7382")); //false.
+console.log(telephoneCheck("10 (757) 622-7382")); //false.
+console.log(telephoneCheck("27576227382")); //false.
+console.log(telephoneCheck("(275)76227382")); //false.
+console.log(telephoneCheck("2(757)6227382")); //false.
+console.log(telephoneCheck("2(757)622-7382")); //false.
+console.log(telephoneCheck("555)-555-5555")); //false.
+console.log(telephoneCheck("(555-555-5555")); //false.
+console.log(telephoneCheck("(555)5(55?)-5555")); //false.
+console.log(telephoneCheck("55 55-55-555-5")); //false.
+console.log(telephoneCheck("11 555-555-5555")); //false.
