efb3b9891fa27ee264b25684f59cd75c5b5af516
Víctor Gomenza <viguitars738@gmail.com>
2024-01-11 22:52:06 -0300

    Create 01.lineal.js

---
+++ Big O Notation/01.lineal.js
@@ -0,0 +1,10 @@
+// Lineal o Primer Orden: O(n)
+
+function linearFunc(arr) {
+	for (let i = 0; i < arr.length; i += 1) {
+		console.log(1000 * 100000);
+	}
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+linearFunc(arr);
